56c6e888a4ae9a8dde89ec2ff4263aa89c3baab1
db360 <da.b@hotmail.es>
2022-05-20 22:53:18 +0200

    credentials, verificar correo y contraseña

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,5 +1,7 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github"
+import Credentials from "next-auth/providers/credentials"
+import { dbUsers } from "../../../database";
 
 
 export default NextAuth({
@@ -7,13 +9,59 @@ export default NextAuth({
 
   // Configure one or more authentication providers
   providers: [
+    Credentials({
+      name: 'Custom Login',
+      credentials: {
+        email: {label: 'Correo', type: 'email', placeholder: 'correo@google.com'},
+        password: {label: 'Contraseña:', type: 'label', placeholder: 'Contraseña'},
+      },
+      async authorize(credentials) {
+
+        console.log(credentials);
+        //TODO: Validar con la base de datos
+
+        // return {name: 'Juan', correo: 'juan@juan.com', role: 'admin'};
+
+        return await dbUsers.checkUserEmailPassword(credentials!.email, credentials!.password);
+      }
+    }),
     GithubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
     }),
-    // ...add more providers here
   ],
-})
 
-console.log(process.env.GITHUB_ID)
-console.log(process.env.GITHUB_SECRET)
+  //Callbacks
+  callbacks: {
+
+    async jwt({token, account, user}) {
+      // console.log({token, account, user});
+
+      if(account) {
+        token.accessToken = account.access_token;
+
+        switch(account.type) {
+          case 'oauth':
+            //TODO: crear usuario o verificar si existe en la bd
+          break;
+
+          case 'credentials':
+            token.user = user;
+          break;
+        }
+      }
+
+      return token;
+    },
+
+    async session({session, token, user}) {
+      // console.log({session, token, user});
+      session.accessToken = token.accessToken;
+      session.user = token.user as any;
+
+      return session;
+    },
+  }
+});
+
+
