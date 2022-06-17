3dd6ed56e5fd791f363df4fb238a7d36cafa3234
db360 <da.b@hotmail.es>
2022-06-17 18:38:07 +0200

    etiquetas del producto

--- pages/api/admin/products.ts
+++ pages/api/admin/products.ts
@@ -1,3 +1,4 @@
+import { isValidObjectId } from 'mongoose';
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { db } from '../../../database';
 import { IProduct } from '../../../interfaces';
@@ -6,6 +7,7 @@ import { Product } from '../../../models';
 type Data =
 |   {message: string;}
 |   IProduct[]
+|   IProduct
 
 export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
 
@@ -14,11 +16,11 @@ export default function handler(req: NextApiRequest, res: NextApiResponse<Data>)
             return getProducts(req, res);
 
         case 'PUT':
-
+            return updateProduct(req, res);
 
         case 'POST':
 
-        
+
         default:
             return res.status(400).json({message: 'Bad Request'});
     }
@@ -38,3 +40,38 @@ const  getProducts = async(req: NextApiRequest, res: NextApiResponse<Data>) => {
 
     res.status(200).json(products);
 }
+const  updateProduct = async(req: NextApiRequest, res: NextApiResponse<Data>) => {
+
+    const { _id = '', images = [] } = req.body as IProduct;
+    if( !isValidObjectId( _id ) ) {
+        return res.status(400).json({ message: 'El Id del producto no es válido' });
+    }
+
+    if( images.length <= 2 ) {
+        return res.status(400).json({ message: 'Se Necesitan al menos dos imágenes' });
+    }
+
+    // todo: Posiblenente tendremos un localhost:3000/products/xxxxx.jpg
+
+    try {
+        await db.connect();
+        const product = await Product.findById(_id);
+        if(!product) {
+            await db.disconnect();
+            return res.status(400).json({ message: 'No existe ningun producto con ese ID' + _id})
+        }
+        // todo: eliminar imagenes en Cloudinary
+
+        await product.update( req.body );
+        await db.disconnect();
+        
+        return res.status(200).json( product )
+
+    } catch (error) {
+
+        console.log(error);
+        await db.disconnect();
+        return res.status(400).json({ message: 'ERROR: Revisar la consola del servidor'})
+    }
+}
+
